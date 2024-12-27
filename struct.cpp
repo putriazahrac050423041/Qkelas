1f6f6cd600bcfc23f80858fb81eb7b01ec73e977
putriazahrac050423041 <azahraptr24@gmail.com>
2024-12-27 10:14:21 +0800

    menambahkan code

--- struct.cpp
+++ struct.cpp
@@ -23,4 +23,21 @@ cout<<"Masukan angka :";
 
 mahasiswa sikc[30];
 int pos=-1;
+void masukanData() {
+    pos++;
+    system("cls");
+    cout << "Masukkan data mahasiswa SIKC-3B ke-" << (pos + 1) << ":" << endl;
+    cin.ignore();
+    cout << "Masukkan NIM: ";
+    getline(cin, sikc[pos].nim);
+    cout << "Masukkan Nama: ";
+    getline(cin, sikc[pos].nama);
+    cout << "Masukkan Alamat: ";
+    getline(cin, sikc[pos].alamat);
+    cout << "Masukkan IPK: ";
+    cin >> sikc[pos].ipk;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    cout << "\nData mahasiswa berhasil ditambahkan!\n";
+    cin.get();
+}
 
